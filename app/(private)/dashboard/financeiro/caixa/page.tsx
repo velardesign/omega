16e5f443eb21a70be43e2c7e2e60fcb5c2e7aab9
Anderson Andrade <212961773+velardesign@users.noreply.github.com>
2026-02-22 19:34:20 -0300

    config: docker + migrate deploy

--- app/(private)/dashboard/financeiro/caixa/page.tsx
+++ app/(private)/dashboard/financeiro/caixa/page.tsx
@@ -1,11 +1,15 @@
 import FinancialOverview from "@/components/dashboard/financial-overview";
-import { Separator } from "@/components/ui/separator";
+import {Separator} from "@/components/ui/separator";
 
 export default function CashFlowPage() {
-  return (
-    <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-      <FinancialOverview />
-      <Separator />
-    </div>
-  );
+    return (
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+            <FinancialOverview valoresCaixa={{
+                total: 0,
+                totalEntradas: 0,
+                totalSaidas: 0,
+            }} valoresTodosCaixas={0}/>
+            <Separator/>
+        </div>
+    );
 }
--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,3 +1,3 @@
 import { createAuthClient } from "better-auth/react"
-//teste
+
 export const authClient = createAuthClient()
