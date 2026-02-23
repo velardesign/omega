6c3a3bbaad53d816be15aeb387e67e512f3e00f0
Anderson Andrade <212961773+velardesign@users.noreply.github.com>
2026-02-22 21:17:31 -0300

    tentando corrigir bug

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,6 +1,3 @@
-import { createAuthClient } from "better-auth/react"
+import {createAuthClient} from "better-auth/react"
 
-export const authClient = createAuthClient({
-        baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL || "http://localhost:3000",
-    }
-)
+export const authClient = createAuthClient();
