2e0714e9e34eac703e13478cda567fb93de0aaef
Anderson Andrade <212961773+velardesign@users.noreply.github.com>
2026-02-22 22:23:14 -0300

    criando logs para descobrir valores da variaveis

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -4,7 +4,9 @@ const authBaseURL =
     process.env.NEXT_PUBLIC_BETTER_AUTH_URL ||
     (typeof window !== "undefined" && window.location.origin) ||
     "http://localhost:3000";
-
+ // ← debug
+console.log("[BetterAuth] VERCEL_URL:", process.env.VERCEL_URL);       // ← debug
+console.log("[BetterAuth] BETTER_AUTH_URL:", process.env.BETTER_AUTH_URL); // ← debug
 export const authClient = createAuthClient({
     baseURL: authBaseURL,
 });
