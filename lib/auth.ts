a6b73cda6517ce9fdd3715b41d757c2e5ce68410
Anderson Andrade <212961773+velardesign@users.noreply.github.com>
2026-02-22 21:30:58 -0300

    tentando resolver bug de logi

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,17 +2,18 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "@/lib/prisma"
 
-const appUrl = process.env.BETTER_AUTH_URL || "http://localhost:3000";
-
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "postgresql",
     }),
     secret: process.env.BETTER_AUTH_SECRET,
-    emailAndPassword:{
-        enabled:true,
-        requireEmailVerification:false,
+    emailAndPassword: {
+        enabled: true,
+        requireEmailVerification: false,
     },
-    baseURL: appUrl,
-    trustedOrigins: [appUrl],
+    baseURL: "https://omega-v46e.vercel.app",
+    trustedOrigins: [
+        "https://omega-v46e.vercel.app",
+        "http://localhost:3000",
+    ],
 });
