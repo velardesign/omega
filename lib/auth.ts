8687f4ebb81b546953a5b60a2ca60282e115fd32
Anderson Andrade <212961773+velardesign@users.noreply.github.com>
2026-02-22 22:39:04 -0300

    brigando contra o bug

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,14 +3,8 @@ import {prismaAdapter} from "better-auth/adapters/prisma";
 import {prisma} from "@/lib/prisma"
 
 const appUrl =
-    process.env.BETTER_AUTH_URL ||
-    process.env.VERCEL_URL
-        ? `https://${process.env.VERCEL_URL}`
-        : "http://localhost:3000";
+    process.env.BETTER_AUTH_URL || "http://localhost:3000";
 
-console.log("[BetterAuth] appUrl calculado:", appUrl);                 // ← debug
-console.log("[BetterAuth] VERCEL_URL:", process.env.VERCEL_URL);       // ← debug
-console.log("[BetterAuth] BETTER_AUTH_URL:", process.env.BETTER_AUTH_URL); // ← debug
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "postgresql",
