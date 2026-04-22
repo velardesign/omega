767483c694a90299c99c7bf73ac5f3978baf123c
Anderson Andrade <212961773+velardesign@users.noreply.github.com>
2026-04-22 17:31:14 -0300

    Adicionada tag do analytics da vercel

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import {Contact} from "@/components/home/contact";
 import {About} from "@/components/home/about";
 import {Gallery} from "@/components/home/gallery";
 import HeroCarousel from "@/components/home/hero-carousel";
-
+import { Analytics } from "@vercel/analytics/next"
 
 export default function Home() {
     return (
@@ -13,6 +13,7 @@ export default function Home() {
             <About/>
             <Gallery/>
             <Contact/>
+            <Analytics />
         </div>
     );
 }
